c1fe12af045b8722b204dd7bccb7d341352ac997
Ian Michael Castronuevo <imcastronuevo@gmail.com>
2023-12-06 03:01:37 +0800

    updated files and folders

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+// server.js
+require('dotenv').config();
+
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const path = require('path');
+const routes = require('./utils/routes');
+const connectToDatabase = require('./utils/database');  // Change the import to use connectToDatabase function
 
 const app = express();
 const port = 3000;
@@ -9,62 +12,31 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Set up static files (HTML, CSS, JS, images)
-app.use(express.static(path.join(__dirname, '')));
+// Use the MongoDB connection from database.js
+const mongooseConnection = connectToDatabase();  // Call the function to establish the connection
 
-// Define routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'contact.html'));
-});
+// Define your routes
+app.use('/', routes);
 
-app.get('/services', (req, res) => {
-    res.sendFile(path.join(__dirname, 'services.html'));
-});
+const FormData = require('./models/formData');
 
+app.post('/submit-form', (req, res) => {
+    const formData = new FormData(req.body);
 
-const mongoURI = 'mongodb+srv://imcastronuevo:sAR880chSW0r1XSc@cluster0.vtj4bqe.mongodb.net/?retryWrites=true&w=majority';
-
-// Connect to MongoDB Atlas
-mongoose.connect(mongoURI);
-
-const db = mongoose.connection;
-
-// schema and model 
-const formDataSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    mobileNumber: String,
-    phoneNumber: String,
-    preferredDatetime: Date,
-});
-
-const FormData = mongoose.model('FormData', formDataSchema);
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB Atlas');
-
-    // Handle form submissions
-    app.post('/submit-form', (req, res) => {
-      const formData = new FormData(req.body);
-    
-      formData.save()
+    formData.save()
         .then(() => {
-          console.log('Form data saved successfully');
-          res.status(200).send('Form data saved successfully');
+            console.log('Form data saved successfully');
+            res.status(200).send('Form data saved successfully');
         })
         .catch((err) => {
-          console.error('Error saving form data:', err);
-          res.status(500).send('Internal Server Error');
+            console.error('Error saving form data:', err);
+            res.status(500).send('Internal Server Error');
         });
-    });
+});
 
-    // Start the server after MongoDB connection is established
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ utils/database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGO_URI;
+
+function connectToDatabase() {
+    const mongooseOptions = {
+        ...(parseInt(mongoose.version.split('.')[0], 10) < 4 && {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }),
+    };
+
+    mongoose.connect(mongoURI, mongooseOptions);
+
+    const db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    db.once('open', () => {
+        console.log('Connected to MongoDB Atlas');
+    });
+
+    return db;
+}
+
+module.exports = connectToDatabase;
