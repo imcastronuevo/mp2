40cf2a49cf577d3d5634d773df41a2c79ddcf729
Ian Michael Castronuevo <imcastronuevo@gmail.com>
2023-12-05 20:47:51 +0800

    updated responsiveness

--- server.js
+++ server.js
@@ -1,69 +1,70 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Use express.json() for parsing JSON requests
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Set up static files (HTML, CSS, JS, images)
 app.use(express.static(path.join(__dirname, '')));
 
 // Define routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'contact.html'));
+    res.sendFile(path.join(__dirname, 'contact.html'));
 });
 
 app.get('/services', (req, res) => {
-  res.sendFile(path.join(__dirname, 'services.html'));
+    res.sendFile(path.join(__dirname, 'services.html'));
 });
 
-// Connect to MongoDB Atlas using environment variable
-mongoose.connect(process.env.MONGODB_URI);
+
+const mongoURI = 'mongodb+srv://imcastronuevo:sAR880chSW0r1XSc@cluster0.vtj4bqe.mongodb.net/?retryWrites=true&w=majority';
+
+// Connect to MongoDB Atlas
+mongoose.connect(mongoURI);
 
 const db = mongoose.connection;
 
-// Schema and model for form data
+// schema and model 
 const formDataSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     mobileNumber: String,
     phoneNumber: String,
     preferredDatetime: Date,
-  });
-  
+});
 
 const FormData = mongoose.model('FormData', formDataSchema);
 
-// Event handling for MongoDB connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB Atlas');
+    console.log('Connected to MongoDB Atlas');
 
-  // Handle form submissions
-  app.post('/submit-form', (req, res) => {
-    const formData = new FormData(req.body);
+    // Handle form submissions
+    app.post('/submit-form', (req, res) => {
+      const formData = new FormData(req.body);
+    
+      formData.save()
+        .then(() => {
+          console.log('Form data saved successfully');
+          res.status(200).send('Form data saved successfully');
+        })
+        .catch((err) => {
+          console.error('Error saving form data:', err);
+          res.status(500).send('Internal Server Error');
+        });
+    });
 
-    formData.save()
-      .then(() => {
-        console.log('Form data saved successfully');
-        res.status(200).send('Form data saved successfully');
-      })
-      .catch((err) => {
-        console.error('Error saving form data:', err);
-        res.status(500).send('Internal Server Error: Could not save form data');
-      });
-  });
-
-  // Start the server after MongoDB connection is established
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
+    // Start the server after MongoDB connection is established
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
 });
